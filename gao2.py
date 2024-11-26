53f90e36031c44cb1d3e6d99bed0cb5ebbd5fb1c
Goo Kim <daekeun7@gmail.com>
2024-11-26 20:22:43 +0900

    Update gao2.py

--- gao2.py
+++ gao2.py
@@ -1,11 +1,24 @@
 import streamlit as st
 from PIL import Image # 위에서 선언 후 사용해야한다.
 
+st.title("Word Cloud of Keywords from GAO Reports since 2022")
 
-# main()
-img = Image.open('images/en/Agriculture and Food.png')
+tabs = st.tabs(["GAO_EN", "GAO_한글"])
 
-# 경로에 있는 이미지 파일을 통해 변수 저장
+with tabs[0]:
+  # main()
+  img = Image.open('images/en/Agriculture and Food.png')
+  
+  # 경로에 있는 이미지 파일을 통해 변수 저장
+  
+  st.image(img)
+  # streamlit를 통해 이미지를 보여준다.
 
-st.image(img)
-# streamlit를 통해 이미지를 보여준다.
+with tabs[1]:
+  # main()
+  img = Image.open('images/en/Agriculture and Food.png')
+  
+  # 경로에 있는 이미지 파일을 통해 변수 저장
+  
+  st.image(img)
+  # streamlit를 통해 이미지를 보여준다.
