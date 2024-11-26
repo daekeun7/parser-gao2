449c224a64af8b5a83e61f9c03f8288634a063d3
Goo Kim <daekeun7@gmail.com>
2024-11-26 20:27:17 +0900

    Update gao2.py

--- gao2.py
+++ gao2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image # 위에서 선언 후 사용해야한다.
 
-st.title("Word Cloud of Keywords from GAO Reports since 2022")
+st.title(""Word Cloud" using GAO reports since 2022")
 
 tabs = st.tabs(["GAO_EN", "GAO_한글"])
 
